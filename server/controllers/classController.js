8730f384821ce65664e74ff4aa2e1534ec43bb21
Kirsten Segavac <kirstensegavac@gmail.com>
2022-07-26 19:52:31 +0800

    update class and delete class (controller) updated

--- server/controllers/classController.js
+++ server/controllers/classController.js
@@ -5,7 +5,7 @@ const Teacher = require('../models/teacher');
 
 // getClass | GET route api/class (private)
 const getClass = asyncHandler(async (req, res) => {
-    const className = await Class.find({ teacher: req.user.id })
+    const className = await Class.find({ teacher: req.teacher.id })
     res.status(200).json(className)
 });
 
@@ -17,13 +17,68 @@ const createClass = asyncHandler(async (req, res) => {
     }
     const className = await Class.create({
         text: req.body.text, // get help with this... is it body.text?
-        user: req.user.id
+        teacher: req.teacher.id
     });
     res.status(200).json(className);
 });
 
 // updateClass | PUT route api/class (private)
+const updateClass = asyncHandler(async (req, res) => {
+    const classes = await Class.findById(req.params.id)
+  
+    if (!goal) {
+      res.status(400)
+      throw new Error('Class not found')
+    }
+  
+    // Check for user/teacher
+    if (!req.teacher) {
+      res.status(401)
+      throw new Error('Teacher not found')
+    }
+  
+    // Make sure the logged in user/teacher matches the class teacher
+    if (classes.teacher.toString() !== req.teacher.id) {
+      res.status(401)
+      throw new Error('Teacher not authorized')
+    }
+  
+    const updatedClass = await Class.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    })
+  
+    res.status(200).json(updatedClass)
+  })
+
+
+  // up to here
+
 // deleteClass | DELETE route api/class (private)
+const deleteClass = asyncHandler(async (req, res) => {
+    const classes = await Class.findById(req.params.id)
+  
+    if (!classes) {
+      res.status(400)
+      throw new Error('Class not found')
+    }
+  
+    // Check for user
+    if (!req.teacher) {
+      res.status(401)
+      throw new Error('Teacher not found')
+    }
+  
+    // Make sure the logged in user/teacher matches the class teacher
+    if (goal.teacher.toString() !== req.teacher.id) {
+      res.status(401)
+      throw new Error('Teacher not authorized')
+    }
+  
+    await classes.remove()
+  
+    res.status(200).json({ id: req.params.id })
+  })
+  
 
 module.exports = {
     getClass,
